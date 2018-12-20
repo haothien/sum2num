633c8a4fa1dbaf3c8b68c78f10fec1f842add8a6
haothien <43365744+haothien@users.noreply.github.com>
2018-12-20 10:24:40 +0700

    Add files via upload

---
+++ src/MyBigMain.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bhhth
+ */
+import java.util.Scanner;
+
+public class MyBigMain {
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MyBigMain main = new MyBigMain();
+        MyBigNumber num = new MyBigNumber();
+
+        System.out.print("Nhập số đầu: ");
+        String str1 = scanner.next();
+
+        System.out.print("Nhập số thứ hai: ");
+        String str2 = scanner.next();
+
+        System.out.println("Kết quả phép cộng là " + num.sum2num(str1, str2));
+    }
+
+    
+}
