b41b472d312e57fe16c9d791ac5da16556cf8b71
haothien <43365744+haothien@users.noreply.github.com>
2018-12-21 18:29:14 +0700

    Update class MyBigNumber

--- src/MyBigNumber.java
+++ src/MyBigNumber.java
@@ -20,14 +20,12 @@ public class MyBigNumber {         // khai bao class MyBigNumber
         String step = "";// chuoi luu cac buoc cua phep tinh
         int rem = 0;// bien luu ki tu so nho se duoc them vao tiep theo
         int gtd = 0; // bien gia tri du(so nho)
-        int temp1 = 0;// bien tam 1
-        int temp2 = 0;// bien tam 2
+        
         int write = 0;// bien nho
-        int vtc1 = 0;// vi tri cuoi cung cua chuoi so 1
-        int vtc2 = 0;// vi tri cuoi cung cua chuoi so 2
-        char digit1 = '0';// bien luu ki tu khi duoc lay ra 
-        char digit2 = '0';// bien luu ki tu khi duoc lay ra
-        try {
+        
+        int digit1 ;// bien luu ki tu khi duoc lay ra 
+        int digit2 ;// bien luu ki tu khi duoc lay ra
+        try{
             if (!s1.matches("^[0-9]+$") || !s2.matches("^[0-9]+$")) {
 
                 throw new NumberFormatException();
@@ -45,43 +43,36 @@ public class MyBigNumber {         // khai bao class MyBigNumber
         }
 
         for (int i = 0; i < max; i++) {
-            vtc1 = str1 - i - 1;
-            vtc2 = str2 - i - 1;
+            digit1 = i < str1 ? (s1.charAt(str1 - i - 1) - '0') : 0;  // update and get the last digit
+            digit2 = i < str2 ? (s2.charAt(str2 - i - 1) - '0') : 0;
 
-            if (vtc1 >= 0) {
-                digit1 = s1.charAt(vtc1); //Lay ki tu cuoi day gan vao bien digit1
-            }
-            if (vtc2 >= 0) {
-                digit2 = s2.charAt(vtc2); //Lay ki tu cuoi day gan vao bien digit2
-            }
+            
 
-            temp1 = digit1 - '0';
-            temp2 = digit2 - '0';
-
-            sum12 = temp2 + temp1; //tong 2 ki tu duoc lay ra
+            sum12 = digit1 + digit2; //tong 2 ki tu duoc lay ra
             write = (sum12 + rem) % 10;
             if (sum12 < 9) {
                 gtd = sum12 / 10;
 
             }
             if (max == 1) {
-                step += "\n" + " Lay " + temp1 + " cong voi " + temp2
+                step += "\n" + " Lay " + digit1 + " cong voi " + digit2
                         + " bang " + sum12 + "\n";
-            } else if (i != 0) {
-                step += "\n" + " Buoc " + (i + 1) + ":\n" + " Lay " + temp1
-                        + " cong voi " + temp2
+            } else if (rem == 0) {
+                step += "\n" + " Buoc " + (i + 1) + ":\n" + " Lay " + digit1
+                        + " cong voi " + digit2
                         + " duoc " + sum12 + "\n"
-                        + " Cong them so nho " + rem
-                        + " ta duoc " + (sum12 + rem) + " \n "
+                        
                         + " Ghi " + write
                         + " nho " + gtd + "\n";
 
             } else {
                 step += "\n" + "Buoc " + (i + 1) + " :\n"
-                        + "Lay " + temp1
-                        + " cong " + temp2
+                        + "Lay " + digit1
+                        + " cong " + digit2
                         + " duoc " + sum12 + "\n"
-                        + "Ghi " + write
+                        + "Cong them so nho " + rem
+                        + " ta duoc " + (sum12 + rem) + " \n "
+                        + " Ghi " + write
                         + " nho " + gtd + "\n";
             }
             rem = sum12 / 10;
